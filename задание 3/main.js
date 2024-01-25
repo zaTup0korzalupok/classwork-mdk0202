6a598c9c18584d6e83b3a37a24c28e3664160fd5
zaTup0korzalupok <degeneratabobyvich228@gmail.com>
2024-01-25 13:37:43 +0300

    js object

--- задание 3/main.js
+++ задание 3/main.js
@@ -1,13 +1,51 @@
 alert("hell")
-let arr=[["h","e","l","l","o"],["w","o","r","l","d"],["Google","Yandex","IBM","Apple","Amd"],["zxc","qwerty","leet","🤙","lol"]]
+let arr=[["h","e","l","l","o"],["w","o","r","l","d"],["Google","Yandex","IBM","Apple","Amd"],["zxc","qwerty","leet","🤙","lol"]] //why js ??? dont read jambo????
 //["a","b","c"]
 
-console.log(arr)
-console.log(arr[1][1])
-console.log(arr[2][2])
-console.log(arr[3][3])
-console.log(arr[4][4])
+//console.log(arr)
+// console.log(arr[1][1])
+// console.log(arr[2][2])
+// console.log(arr[3][3])
+// console.log(arr[4][4])
+let obj={
+    age:30,
+    name:"♂️ Billy ♂️",
+    subname:"♂️ Herrington ♂️",
+    money:300,//♂️
+    role:"♂️ boss gym ♂️",
+    zxc1:[1,2,3],
+    obi:{word:"♂️ goida ♂️",pose:"♂️♂️♂️♂️♂️"},
+    asdf:function zxc2(){console.log(arr[3][4])}
+}
+console.log(obj.name)
+console.log(obj.obi.word)
+console.log(obj.zxc1[1])
+console.log(obj.asdf())
 
+let col={
+    students:{
+        group:12,
+        course:2,
+        name:"azaza",
+        subname:"aazaa",
+        data:["other data array ",["h","e","l","l","o"],["w","o","r","l","d"],],
+        phone:1137
+    },
+    techer:{
+        name:"aazaa",
+        subname:"aazaa",
+        otherdata:["other data array ",["h","e","l","l","o"],["w","o","r","l","d"],],
+        phone:1137
+
+    },
+    adres:"azazazzaa",
+    phone:133722896,
+    email:"mail@mail.com"
+}
+
+console.log(col)
+console.log(col.students.data[0])
+console.log(col.students.data[1][1])
 // console.log(arr[1])
 // arr[1]="n"
 // console.log(arr[1])
