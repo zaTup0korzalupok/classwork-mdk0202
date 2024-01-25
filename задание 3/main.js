41adf08d3dca363e42dabaf35ed9f782f30079af
zaTup0korzalupok <degeneratabobyvich228@gmail.com>
2024-01-25 10:30:02 +0300

    array js

---
+++ задание 3/main.js
@@ -0,0 +1,15 @@
+alert("hell")
+let arr=[["h","e","l","l","o"],["w","o","r","l","d"],["Google","Yandex","IBM","Apple","Amd"],["zxc","qwerty","leet","🤙","lol"]]
+//["a","b","c"]
+
+console.log(arr)
+console.log(arr[1][1])
+console.log(arr[2][2])
+console.log(arr[3][3])
+console.log(arr[4][4])
+
+// console.log(arr[1])
+// arr[1]="n"
+// console.log(arr[1])
+// arr[4]="z"
+// console.log(arr[4])
