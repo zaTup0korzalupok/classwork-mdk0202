7207f662a3551b7a7b33b6745ebb740c5ec246ae
zaTup0korzalupok <degeneratabobyvich228@gmail.com>
2024-01-26 13:37:29 +0300

    include

--- задание 4/main.js
+++ задание 4/main.js
@@ -9,22 +9,22 @@
 // }
 // let zxc = filter(arr,27)
 // console.log(zxc)
-let arr=[{ age:30,name:"♂️ Billy ♂️",subname:"♂️ Herrington ♂️",group:"1"},
-{ age:27,name:"♂️ john ♂️", subname:"♂️ doe ♂️",group:"1"},
-{ age:32,name:"♂️ van ♂️", subname:"♂️ dark ♂️",group:"4"},
-{ age:30,name:"♂️ Billy ♂️",subname:"♂️ Herrington ♂️",group:"4"},
-{ age:27,name:"♂️ john ♂️", subname:"♂️ doe ♂️",group:"3"},
-{ age:32,name:"♂️ van ♂️", subname:"♂️ dark ♂️",group:"1"},
-{ age:32,name:"♂️ Billy ♂️",subname:"♂️ Herrington ♂️",group:"2"},
-{ age:27,name:"♂️ john ♂️", subname:"♂️ doe ♂️",group:"3"},
-{ age:32,name:"♂️ van ♂️", subname:"♂️ dark ♂️",group:"2"},
-{ age:32,name:"♂️ john ♂️", subname:"♂️ doe ♂️",group:"3"}
+let arr=[{ age:30,name:"♂️ Billy ♂️",subname:"♂️ Herrington ♂️",group:"1",  zxc1:[,2,]},
+{ age:27,name:"♂️ john ♂️", subname:"♂️ doe ♂️",group:"1",  zxc1:[1,2,]},
+{ age:32,name:"♂️ van ♂️", subname:"♂️ dark ♂️",group:"4",  zxc1:[1,,3]},
+{ age:30,name:"♂️ Billy ♂️",subname:"♂️ Herrington ♂️",group:"4",  zxc1:[1,2,]},
+{ age:27,name:"♂️ john ♂️", subname:"♂️ doe ♂️",group:"3",  zxc1:[1,2,]},
+{ age:32,name:"♂️ van ♂️", subname:"♂️ dark ♂️",group:"1",  zxc1:[1,,3]},
+{ age:32,name:"♂️ Billy ♂️",subname:"♂️ Herrington ♂️",group:"2",  zxc1:[,2,]},
+{ age:27,name:"♂️ john ♂️", subname:"♂️ doe ♂️",group:"3",  zxc1:[1,,]},
+{ age:32,name:"♂️ van ♂️", subname:"♂️ dark ♂️",group:"2",  zxc1:[,2,3]},
+{ age:32,name:"♂️ john ♂️", subname:"♂️ doe ♂️",group:"3",  zxc1:[1,,]}
 ]//omg function 
 function filter(targetArr,group){
 
-    return targetArr.filter(person=>person.group ==group)
+    return targetArr.filter(person=>person.zxc1.imcludes==group)
 
 }
 let zxc = filter(arr,3)
 console.log(zxc)
-console.log(zxc.name)
+
