624b92ea9386cb858104e328eb566ab6403afe27
Pavel016 <nosachov2004@gamail.com>
2024-02-01 09:47:56 +0300

    help

---
+++ задание 6/main.js
@@ -0,0 +1,46 @@
+// function main(){
+// console.log('привет, я функция');
+// }
+// main()
+// main()
+// main()
+// function zxc(z,x){
+//     console.log(z+x);
+//  }
+//  zxc(1,2)
+//  function student(firsName,lastName,group='303prg'){
+//     alert(`привет, ${firsName}${lastName}.я знаю, что ты из группы ${group}`)
+//  }
+//  let fname = prompt('введите сове имя')
+//  let iname = prompt('ВВЕДИТЕ ФАМИЛИЮ')
+//  let sGroup = prompt('введите название группы')
+//  student('англосакс', 'фурри');
+//  student(fname,iname,sGroup)
+//  function person(pers){
+// console.log(`pers[0]`)
+// console.log(`pers[1]`)
+// console.log(`pers[2]`)
+//  }
+//  let arr = [1,'tom@30',[1,2,3]]
+//  person(arr)
+function main(a,b=2){
+let res = a*b
+// console.log(res)o
+return res
+}
+let x =main(2,3) 
+let y = x+2
+
+console.log(y)
+function cock(z,v){
+if(z>v){
+console.log(`значение z = ${z} больше значения v = ${v}`)
+}else if (v>z){
+console.log(`значение v = ${v} больше значения z = ${z}`)
+}else if (z === undefined || v === undefined){
+ console.log('вы не ввели значения')
+} else {
+ console.log('введите корректные значение ')
+}
+}
+cock(sosi)
