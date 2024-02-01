3c400eb112308d29b1ba488a1e88a209481548e0
Pavel016 <nosachov2004@gamail.com>
2024-02-01 11:25:39 +0300

    wtf

---
+++ задание 7/main.js
@@ -0,0 +1,67 @@
+//1
+alert("hello")
+let arr =[1,2,3,4,5,6,7,8,9,10]
+function zxc(x){
+    for(element in x){
+        const element = array[index];
+        if(element %2==0 ){
+       
+            let z = element
+            console.log(z)
+        }
+
+    }
+}
+zxc(arr)
+//2
+let foo=[]
+for (let i = 1; i <= 100; i++){
+    if(i%3 ===0 && i % 5 ===0 && i %7=== 0){
+        console.log(i);
+    }
+}
+
+//3
+const getLargestExpressionResult = (a, b) => {
+
+    let sum = a + b;
+    let razn = a - b;
+    let mul = a * b;
+    let del = a / b;
+  
+     if(sum > (razn && mul && del)){
+       return sum;
+    }
+     if(razn > (sum && mul && del)){
+       return  razn;
+    }
+    if(mul > (sum && razn && del)){
+       return mul;
+    }
+    if(del > (sum && razn && mul)){
+       return del;
+    }
+  }
+//4
+function sum(x){
+    let z 
+    for(element in x){
+      z+= element
+
+}
+console.log(z)
+}
+let a=[1,2,3]
+sum(a)
+//5
+// const fagg
+// fagg[1,4,1,7,8]
+function i(){
+    // if (){
+
+    // } else{
+
+    // } 
+    
+}
+//6
