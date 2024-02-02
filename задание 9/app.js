dbfe00255178131d6139ed113048f67c836e3535
Pavel016 <nosachov2004@gmail.com>
2024-02-02 13:38:02 +0300

    help me

---
+++ задание 9/app.js
@@ -0,0 +1,80 @@
+class sum{
+    x
+    y
+    z
+    result
+    constructor(varX,varY,varZ){
+        this.x = varX
+        this.y = varY
+        this.z = varZ
+    }
+    print(){
+        this.result = this.x + (this.y * this.z)
+        console.log(`ваш результат - ${this.result}`)
+
+    }
+}
+const result = new sum(2,3,4)
+result.print()
+//dz
+class ZXC{
+    a
+    b
+    c
+    r
+    m
+    d
+
+    constructor(AA,BB){
+        this.a=AA
+        this.b=BB
+    }
+    name() {
+        this.c = this.a + this.b;
+         this.r = this.a - this.b;
+         this.m = this.a * this.b;
+         this.d = this.a / this.b;
+      
+         if(sum > (razn && mul && del)){
+           return sum;
+        }
+         if(razn > (sum && mul && del)){
+           return  razn;
+        }
+        if(mul > (sum && razn && del)){
+           return mul;
+        }
+        if(del > (sum && razn && mul)){
+           return del;
+        }
+    }
+}
+
+//4
+
+class z{
+    w
+    e
+    r
+    constructor(x){
+        this.w = x
+    }
+    foo(){
+        
+    }
+}
+
+//5
+
+//5
+// const fagg =[1,4,-1,7,8,3,6]
+// function i5(x){
+//     if (){
+        
+
+//     } else{
+
+//     } 
+    
+//}
+i5()
