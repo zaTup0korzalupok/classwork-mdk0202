9bc5d85664691a930adce63c27688d7583c704a5
EvelinaK1 <evelinago.pro@gmail.com>
2024-02-03 17:40:29 +0300

    help2.2

--- задание 9/app.js
+++ задание 9/app.js
@@ -1,80 +1,137 @@
-class sum{
-    x
-    y
-    z
-    result
-    constructor(varX,varY,varZ){
-        this.x = varX
-        this.y = varY
-        this.z = varZ
+// class sum{
+//     x
+//     y
+//     z
+//     result
+//     constructor(varX,varY,varZ){
+//         this.x = varX
+//         this.y = varY
+//         this.z = varZ
+//     }
+//     print(){
+//         this.result = this.x + (this.y * this.z)
+//         console.log(`ваш результат - ${this.result}`)
+
+//     }
+// }
+// const result = new sum(2,3,4)
+// result.print()
+// //dz
+// class ZXC{
+//     a
+//     b
+//     c
+//     r
+//     m
+//     d
+
+//     constructor(AA,BB){
+//         this.a=AA
+//         this.b=BB
+//     }
+//     name() {
+//         this.c = this.a + this.b;
+//          this.r = this.a - this.b;
+//          this.m = this.a * this.b;
+//          this.d = this.a / this.b;
+      
+//          if(c> (r && mul && d)){
+//            return sum;
+//         }
+//          if(r > (c&& m && d)){
+//            return  razn;
+//         }
+//         if(m > (c && r && d)){
+//            return mul;
+//         }
+//         if(d > (c && r&& m)){
+//            return d;
+//         }
+//     }
+// }
+//let abr = new ZXC(1,2)
+// //4
+
+
+//5
+    // const fagg =[1,4,-1,7,8,3,6]
+    // class qwerty{
+    //     arr
+    //     constructor(mass){
+    //         this.arr = mass
+    //     }
+    // leet(){
+    //         for (let index = 0; index < this.arr.length; index++) {
+    //         let zxc = this.arr[index]
+    //             if(zxc <=0){
+    //             console.log("Welcome to Programiz2!");
+    //             }else{
+    //                 console.log("Welcome to Programiz1!");
+    //             }
+    //         }
+    //     }
+    // }
+    // let zxc = new qwerty(fagg)
+    // zxc.leet()
+//5
+    const fagg =[1,4,-1,7,8,3,6]
+    class qwerty{
+        arr
+        constructor(mass){
+            this.arr = mass
+        }
+    leet(){
+            for (let index = 0; index <this.arr.langhyt; index++) {
+             let  element = this.arr[index];
+               
+            }
+            if(zxc <=0){
+                            console.log("Welcome to Programiz2!");
+                            }else{
+                                console.log("Welcome to Programiz1!");
+                            }
+    }
+    }
+    let zxc = new qwerty(fagg)
+    zxc.leet()
+
+    //1
+
+    //2
+    let foo=[]
+    class Two{
+        aboba
+        constructor(jkl){
+            this.aboba = jkl;
+        }
+        star(){
+        for (let i = 1; i <= 100; i++){
+            this.aboba .push() 
+            if(i%3 ===0 && i % 5 ===0 && i %7=== 0){
+                console.log(i);
+            }
+        }
+        console.log(this.aboba );
+}
+}
+let balkan = new Two(foo)
+balkan.star()
+//idk
+class FilterArrays{
+    inputNumber
+    resultFlter
+    constructor(numbers){
+        this.inputNumber = numbers
+    }
+    filter(){
+        return this.inputNumber.filter(number=>(number%3==0&& number%5==0&& number%7==0))
     }
     print(){
-        this.result = this.x + (this.y * this.z)
-        console.log(`ваш результат - ${this.result}`)
-
+        this.resultFlter=this.filter()
+        console.log(this.resultFlter)
     }
 }
-const result = new sum(2,3,4)
-result.print()
-//dz
-class ZXC{
-    a
-    b
-    c
-    r
-    m
-    d
-
-    constructor(AA,BB){
-        this.a=AA
-        this.b=BB
-    }
-    name() {
-        this.c = this.a + this.b;
-         this.r = this.a - this.b;
-         this.m = this.a * this.b;
-         this.d = this.a / this.b;
-      
-         if(sum > (razn && mul && del)){
-           return sum;
-        }
-         if(razn > (sum && mul && del)){
-           return  razn;
-        }
-        if(mul > (sum && razn && del)){
-           return mul;
-        }
-        if(del > (sum && razn && mul)){
-           return del;
-        }
-    }
-}
-
-//4
-
-class z{
-    w
-    e
-    r
-    constructor(x){
-        this.w = x
-    }
-    foo(){
-        
-    }
-}
-
-//5
-
-//5
-// const fagg =[1,4,-1,7,8,3,6]
-// function i5(x){
-//     if (){
-        
-
-//     } else{
-
-//     } 
-    
-//}
-i5()
+let qwe = [1,2,3,4,5,6,7,8,9,10,11]
+let res = new FilterArrays(qwe)
+res.print()
+//6
