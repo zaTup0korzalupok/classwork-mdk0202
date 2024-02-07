d4d82d27f69d85fd7b1bb80f9dc5e6ef89eb220e
ataman-cool134 <nosachov2004@gmail.com>
2024-02-07 09:05:10 +0300

    very sad

--- задание 9/app.js
+++ задание 9/app.js
@@ -135,3 +135,44 @@ let qwe = [1,2,3,4,5,6,7,8,9,10,11]
 let res = new FilterArrays(qwe)
 res.print()
 //6
+//1
+// function factorial(n){
+//     var f = "это единственное место в коде, которое мы имеем право изменить";
+//     f = f.replace("$", n);
+//     for(let i = 0; i < n; i++){
+//         if(parseInt(f)){
+//             throw new Error("Cheaters are not allowed");
+//         }
+//         f = eval(f);
+//     }
+//     return parseInt(f);
+// }
+//2
+// function fn(n){
+//         for(var s=i=1;i<n;s+=(i+=3)){};
+//     return s;
+//     }  
+//     alert(fn(7));
+    
+//3
+//y = ax^2 + bx + c
+let quadraticEquation = (a, b, c) => {
+    if(a == 0)
+        return false;
+    let res = {};
+    let D = b * b - 4 * a * c;
+    console.log('D = ' + D);
+    if(D < 0)
+        return false;
+    res['discriminant'] = D;
+    if(D == 0)
+        res["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+    else if(D > 0){
+        let tmp = [];
+        tmp.push((-b + Math.sqrt(D)) / (2 * a));
+        tmp.push((-b - Math.sqrt(D)) / (2 * a));
+        res["quadratic roots"] = tmp;
+    }
+    return res;
+}
+console.log(quadraticEquation(1,12,36));
