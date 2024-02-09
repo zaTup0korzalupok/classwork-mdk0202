e946206e88a8ad6e24df23716ad6ccad6ff740e2
Pavel016 <nosachov2004@gmail.com>
2024-02-09 13:35:17 +0300

    idk

--- задание 11/app.js
+++ задание 11/app.js
@@ -1,2 +1,8 @@
 const text = document.querySelector('.text')
-console.log(text.innerText)
+let res = text.innerText
+function pox(x,y,z){
+let sum = x*(y+z)
+console.log(sum)
+console.log(z)
+}
+// let byb = 
