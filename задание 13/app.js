c284943982db9dd9c85b0bbc3375e67c84d37f1a
ataman-cool134 <lil_froze04@mail.ru>
2024-02-15 13:41:10 +0300

    god pls kill me

--- задание 13/app.js
+++ задание 13/app.js
@@ -1,42 +1,55 @@
-let FirstName 
-let LastName 
-let nextBtn 
-let reqInput = () => { 
-    FirstName = document.querySelector('#input-fistname').value 
-    LastName = document.querySelector('#input-lastname').value 
-    nextBtn = document.querySelector('#next-btn') 
-    if (FirstName.lenght != 0 && LastName != 0) { 
-        nextBtn.disabled = false 
-    } 
-    return [FirstName, LastName] 
-} 
-let nextFor = () => { 
-    inputChecks = document.querySelector('.wrapper-form-check') 
-    if (inputChecks[0].checked === true && inputChecks[1].checked === false) { 
-        let contentPositive = document.querySelector('.wrapper-content-positive') 
-        contentPositive.style.display = 'block' 
-        return inputChecks 
-    }else{ 
-        alert('null') 
-    } 
-} 
-let saveform =()=>{ 
-    let check = nextForm() 
-    let peopleNames  = reqInput() 
-    if(check[0].checked){ 
-        let contentNegative = document.querySelector('.wrapper-content-positive') 
-         let inputKaf = contentNegative.querySelector('#input-kaf').value 
-         let inputSchool = contentNegative.querySelector('#input-school').value 
-         let select =  contentNegative.querySelector('#select-map') 
-         let selectOptions = select.options[select.selectedIndex].text 
-         alert(`учасьние ${peopleNames[0]} ${peopleNames[1]}, прошел обучение в школе ${inputSchool} и яявляетс\ студентом ${inputKaf} и там ${selectOptions}`) 
- 
-    }else if(check[1].checked){ 
-        let contentNegative =document.querySelector('.wrapper-content-negative') 
-        let inputChild = contentNegative.querySelector('#input-name').value 
-        let inputSchooll = contentNegative.querySelector('input-school').value 
-        let select = contentNegative.querySelector('#select-nap') 
-        let selectOptions = select.options[select.selectedIndex].text 
-        alert(`участник ${peopleNames[0]} ${peopleNames[1]} с наставником ${inputChild}, прошел обучение в школе ${inputSchooll} ${selectOptions}`) 
-    } 
+let firstName
+let lastName
+let nextBtn
+
+let reqInput = () => {
+    firstName = document.querySelector("#input-firstname").value 
+    lastName = document.querySelector("#input-lastname").value
+    nextBtn = document.querySelector("#next-btn")
+
+    if ( firstName.length !=0 && lastName.length !=0 ) {
+        nextBtn.disabled = false
+    }
+    return [ firstName, lastName]
+}
+
+let nextForm = () => {
+    inputChecks = document.querySelectorAll('.wrapper-form-check')
+
+    if ( inputChecks[0].checked === true && inputChecks[1].checked === false) {
+
+        let contentPositive = document.querySelector('.wrapper-content-positive')
+        contentPositive.style.display = 'block'
+        return inputChecks
+    }  else if (inputChecks[1].checked === true && inputChecks[0].checked === false) {
+        
+        let contentNegative = document.querySelector('.wrapper-content-negative')
+        contentNegative.style.display = 'block'
+        return inputChecks
+    } else {
+        alert('Укажите только одно значение')
+    }
+}
+
+let saveForm = () => {
+    let check = nextForm()
+    let peopleNames = reqInput()
+        if ( check[0].checked ) {
+            let contentNegative = document.querySelector('.wrapper-content-positive')
+            let inputKaf = contentNegative.querySelector('#input-kaf').value
+            let inputSchool = contentNegative.querySelector('#input-school').value
+            let select = contentNegative.querySelector('#select-nap')
+            let selectOptions = select.options[select.selectedIndex].text
+            alert(`Участник ${peopleNames[0]} ${peopleNames[1]}, прошел обучение в школе ${inputSchool} и на данный момент является студентом кафедры ${inputKaf} явился на чемпионат по направлению ${selectOptions}`)
+
+        } else if ( check[1].checked) {
+            let contentNegative = document.querySelector('.wrapper-content-negative')
+            let inputChild = contentNegative.querySelector('#input-name').value
+            let inputSchool = contentNegative.querySelector('#input-school').value
+            let select = contentNegative.querySelector('#select-nap')
+            let selectOptions = select.options[select.selectedIndex].text
+            
+            alert(`Участник ${peopleNames[0]} ${peopleNames[1]} с наставником ${inputChild}, прошел обучение в школе ${inputSchool} и явился на чемпионат по направлению ${selectOptions}`)
+
+        }
 }
